368d77ed36ffa26b578541607b14acbefaf970f8
Leeseul-Moon <leeseulmoon4@gmail.com>
2022-08-30 19:53:15 +0900

    modify : MainPage css

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,22 +4,32 @@ import MainButton from "../mainButton/MainButton";
 
 const Search = ({ searchPost }) => {
   const [search, setSearch] = useState({ select: "", input: "" });
+  const [infoText, setInfoText] = useState(false);
 
   const selectRef = useRef();
   const inputRef = useRef();
 
   const onChange = (event) => {
-    if (event.currentTarget == null) {
-      return;
-    }
     event.preventDefault();
     setSearch({
       select: selectRef.current.value,
-      input: inputRef.current.value,
+      input: inputRef.current.value.trim(),
     });
   };
 
+  const info = () => {
+    if (inputRef.current.value === "") {
+      setInfoText(true);
+      setTimeout(() => {
+        setInfoText(false);
+      }, 2000);
+    } else {
+      setInfoText(false);
+    }
+  };
+
   const onSearch = () => {
+    // info();
     searchPost(search);
   };
 
@@ -27,6 +37,7 @@ const Search = ({ searchPost }) => {
     inputRef.current.addEventListener("keypress", logKey);
     function logKey(event) {
       if (event.code === "Enter") {
+        info();
         searchPost(search);
       }
     }
@@ -34,6 +45,7 @@ const Search = ({ searchPost }) => {
 
   return (
     <SearchContainer>
+      <Span text={infoText ? "block" : "none"}>검색어를 입력하세요</Span>
       <SearchSelect ref={selectRef} name="select" onChange={onChange}>
         <option value="title">제목</option>
         <option value="content">내용</option>
@@ -45,15 +57,24 @@ const Search = ({ searchPost }) => {
         ref={inputRef}
         onChange={onChange}
       />
+
       <MainButton onClick={onSearch} children="검색" />
     </SearchContainer>
   );
 };
 
+const Span = styled.div`
+  /* width: 50%; */
+  color: red;
+  display: ${(props) => props.text};
+  position: absolute;
+  bottom: 3em;
+`;
+
 const SearchContainer = styled.div`
   display: flex;
-  width: 40%;
-  margin-top: 0.5em;
+  width: 50%;
+  margin-top: 2em;
   margin-bottom: 0.5em;
   bottom: 0;
 `;
@@ -64,7 +85,7 @@ const SearchSelect = styled.select`
 `;
 
 const SearchInput = styled.input`
-  flex: 1 1 80%;
+  flex: 1 1 50%;
   margin-right: 1em;
 `;
 
