25df7dadab4b8d90b2e14d17e695943cfce42169
kwakhyun <khyun2348@naver.com>
2022-09-02 04:28:21 +0900

    Feat: response 상태에 따른 메시지 추가

--- src/redux/modules/postSlice.js
+++ src/redux/modules/postSlice.js
@@ -2,24 +2,49 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const getPost = createAsyncThunk("GET_POST", async () => {
-  const res = await axios.get(`${process.env.REACT_APP_URL}/posts`);
-  return res.data;
+  const response = await axios.get(`${process.env.REACT_APP_URL}/posts`);
+  return response.data;
 });
 
 export const addPost = createAsyncThunk("ADD_POST", async (post) => {
-  const res = await axios.post(`${process.env.REACT_APP_URL}/posts/`, post);
-  return res.data;
+  const response = await axios.post(
+    `${process.env.REACT_APP_URL}/posts/`,
+    post
+  );
+
+  if (response.status !== 201) {
+    alert("게시글 작성에 실패했습니다.");
+  } else {
+    return response.data;
+  }
 });
 
 export const deletePost = createAsyncThunk("DELETE_POST", async (id) => {
-  await axios.delete(`${process.env.REACT_APP_URL}/posts/` + id);
+  const response = await axios.delete(
+    `${process.env.REACT_APP_URL}/posts/` + id
+  );
+
+  if (response.status !== 200) {
+    alert("게시글 삭제에 실패했습니다.");
+  } else {
+    alert("게시글이 삭제되었습니다.");
+  }
 });
 
 export const updatePost = createAsyncThunk("UPDATE_POST", async (post) => {
-  await axios.patch(`${process.env.REACT_APP_URL}/posts/` + post.id, {
-    title: post.title,
-    content: post.content,
-  });
+  const response = await axios.patch(
+    `${process.env.REACT_APP_URL}/posts/` + post.id,
+    {
+      title: post.title,
+      content: post.content,
+    }
+  );
+
+  if (response.status !== 200) {
+    alert("게시글 수정에 실패했습니다.");
+  } else {
+    alert("게시글이 수정되었습니다.");
+  }
 });
 
 const postSlice = createSlice({
